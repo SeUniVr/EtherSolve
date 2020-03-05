1ae2094ac2e8d1010d699af35ac2409a3b4cdbf7
marcocrosara96 <marcocrosara96@gmail.com>
2020-03-05 18:36:10 +0100

    First version of an alternative "GUI" implemented: it simple show an image with graphviz rendered graph. Actually it does not use Java FX.

---
+++ src/main/java/oldgui/GVBlock.java
@@ -0,0 +1,36 @@
+package oldgui;
+
+import parseTree.BasicBlock;
+
+public class GVBlock {
+    private final BasicBlock mBasicBlock;
+
+    private GVBlock(){
+        this(new BasicBlock());
+    }
+
+    public GVBlock(BasicBlock mBasicBlock) {
+        this.mBasicBlock = mBasicBlock;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GVBlock that = (GVBlock) o;
+        return mBasicBlock.equals(that.mBasicBlock);
+    }
+
+    @Override
+    public int hashCode() {
+        return mBasicBlock.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "\"" +
+                mBasicBlock.toString().replace("\n", "\\l") + "\\l" +
+                mBasicBlock.getStackBalance() +
+                "\"";
+    }
+}
---
+++ src/main/java/oldgui/GVGraph.java
@@ -0,0 +1,64 @@
+package oldgui;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GVGraph {
+    private static final String GRAPH_NAME = "G";
+    private static final String PROPRIETIES = "rankdir=UD;";
+    private static final String DEFAULT_NODE_STYLE = "shape=box style=filled color=black fillcolor=gray " +
+                                                    "fontname=arial fontcolor=black";
+    private static final String FINAL_STATE_NODE_STYLE = "shape = doublecircle, color=black, fontcolor=black";
+
+    Set<GVBlock> blocks;
+    Set<GVEdge> edges;
+
+    public GVGraph() {
+        blocks = new HashSet<>();
+        edges = new HashSet<>();
+    }
+
+    public void addBlock(GVBlock block){
+        blocks.add(block);
+    }
+
+    public void addEdge(GVEdge edge){
+        blocks.add(edge.getFrom());
+        blocks.add(edge.getTo());
+        edges.add(edge);
+    }
+
+    public void addEdge(GVBlock from, GVBlock to){
+        GVEdge edge = new GVEdge(from, to);
+        addEdge(edge);
+    }
+
+    public Set<GVEdge> getEdges() {
+        return edges;
+    }
+
+    public Set<GVBlock> getBlocks(){
+        return blocks;
+    }
+
+    @Override
+    public String toString() {
+        String s = "digraph " + GRAPH_NAME + " {\n" +
+                PROPRIETIES; //+ "\n" +
+                //"node [shape = point, color=white, fontcolor=white]; start;\n";
+
+        s += "\nnode [" + DEFAULT_NODE_STYLE + "];\n";
+        //Blocks are added
+        for (GVBlock block : blocks) {
+            s += block + ";";
+        }
+
+        //Edges are added
+        for (GVEdge edge : edges) {
+            s += edge;
+        }
+
+        s += "}";
+        return s;
+    }
+}
