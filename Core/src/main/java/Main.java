1d0115cc02c550df7417f28c30eedd9c20153be8
VersusF <controfilippo8@gmail.com>
2020-05-13 12:08:36 +0200

    Change sdk to jdk

--- Core/src/main/java/Main.java
+++ Core/src/main/java/Main.java
@@ -5,9 +5,8 @@ import utils.JsonExporter;
 public class Main {
     public static void main(String[] args) {
         String bytecode = "6060608052";
-        Contract contract = null;
         try {
-            contract = new Contract("Pippo", bytecode);
+            Contract contract = new Contract("Pippo", bytecode);
             System.out.println(new JsonExporter().toJson(contract));
         } catch (NotSolidityContractException e) {
             e.printStackTrace();
