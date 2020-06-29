c6a51f7adf1488c4e533e0b84577f062c691af08
VersusF <controfilippo8@gmail.com>
2020-06-29 09:59:43 +0200

    Fix in contract metadata when the bytecode has the 'experimental' tag

--- Core/src/main/java/parseTree/Contract.java
+++ Core/src/main/java/parseTree/Contract.java
@@ -71,12 +71,26 @@ public class Contract {
      */
     private Pair<String, String> removeCompilationInfo(String binary) {
         // From version solc-0.4.17
-        // 0xa1 0x65 'b' 'z' 'z' 'r' '0' 0x58 0x20 <32 bytes swarm hash> 0x00 0x29
+        // 0xa1
+        // 0x65 'b' 'z' 'z' 'r' '0' 0x58 0x20 <32 bytes swarm hash>
+        // 0x00 0x29
         if (binary.matches("^[0-9a-f]*a165627a7a72305820[0-9a-f]{64}0029[0-9a-f]*$")){
             solidityVersion = SolidityVersion.FROM_0_4_17_TO_0_5_8;
             String[] splitString = binary.split("(?<=a165627a7a72305820[0-9a-f]{64}0029)", 2);
             metadata = binary.substring(splitString[0].length() - 86);
             return new Pair<>(splitString[0].substring(0, splitString[0].length() - 86), splitString[1]);
+        }
+        // Experimental option in Solidity due to ABIEncoderV2
+        // example: a265627a7a723058201e1bfc77d507025cf70760b0848f01673dd1fb26af9d47b555da548df16224066c6578706572696d656e74616cf50037
+        // 0xa2
+        // 0x65 'b' 'z' 'z' 'r' '0' 0x58 0x20 <32 bytes swarm hash>
+        // 0x6c 'e' 'x' 'p' 'e' 'r' 'i' 'm' 'e' 'n' 't' 'a' 'l' 0xf5
+        // 0x00 0x37
+        else if (binary.matches("^[0-9a-f]*a265627a7a72305820[0-9a-f]{64}6c6578706572696d656e74616cf50037[0-9a-f]*$")){
+            solidityVersion = SolidityVersion.FROM_0_4_17_TO_0_5_8_EXPERIMENTAL;
+            String[] splitString = binary.split("(?<=a265627a7a72305820[0-9a-f]{64}6c6578706572696d656e74616cf50037)", 2);
+            metadata = binary.substring(splitString[0].length() - 114);
+            return new Pair<>(splitString[0].substring(0, splitString[0].length() - 114), splitString[1]);
         }
 
         // From version solc-0.5.9
@@ -90,6 +104,19 @@ public class Contract {
             metadata = splitString[0].substring(splitString[0].length() - 104);
             return new Pair<>(splitString[0].substring(0, splitString[0].length() - 104), splitString[1]);
         }
+        // Experimental option in Solidity due to ABIEncoderV2
+        // example: a365627a7a7230582022316da6de015a68fad6ca8a732898f553832e95b48e9f39b85fe694b2264db26c6578706572696d656e74616cf564736f6c634300050a0040
+        // 0xa3
+        // 0x65 'b' 'z' 'z' 'r' '0' 0x58 0x20 <32 bytes swarm hash>
+        // 0x6c 'e' 'x' 'p' 'e' 'r' 'i' 'm' 'e' 'n' 't' 'a' 'l' 0xf5
+        // 0x64 's' 'o' 'l' 'c' 0x43 <3 byte version encoding>
+        // 0x00 0x40
+        else if (binary.matches("^[0-9a-f]*a365627a7a72305820[0-9a-f]{64}6c6578706572696d656e74616cf564736f6c6343[0-9a-f]{6}0040[0-9a-f]*$")){
+            solidityVersion = SolidityVersion.FROM_0_5_9_TO_0_5_11_EXPERIMENTAL;
+            String[] splitString = binary.split("(?<=a365627a7a72305820[0-9a-f]{64}6c6578706572696d656e74616cf564736f6c6343[0-9a-f]{6}0040)", 2);
+            metadata = splitString[0].substring(splitString[0].length() - 132);
+            return new Pair<>(splitString[0].substring(0, splitString[0].length() - 132), splitString[1]);
+        }
 
         // From version solc-0.5.12
         // 0xa2
@@ -101,6 +128,19 @@ public class Contract {
             String[] splitString = binary.split("(?<=a265627a7a72315820[0-9a-f]{64}64736f6c6343[0-9a-f]{6}0032)", 2);
             metadata = splitString[0].substring(splitString[0].length() - 104);
             return new Pair<>(splitString[0].substring(0, splitString[0].length() - 104), splitString[1]);
+        }
+        // Experimental option in Solidity due to ABIEncoderV2
+        // example: a365627a7a7231582076f04f08ed9ab2d9078ead8a728e5e444700aed42abb0cd3bd94a1ae5612d38f6c6578706572696d656e74616cf564736f6c63430005110040
+        // 0xa3
+        // 0x65 'b' 'z' 'z' 'r' '1' 0x58 0x20 <32 bytes swarm hash>
+        // 0x6c 'e' 'x' 'p' 'e' 'r' 'i' 'm' 'e' 'n' 't' 'a' 'l' 0xf5
+        // 0x64 's' 'o' 'l' 'c' 0x43 <3 byte version encoding>
+        // 0x00 0x40
+        else if (binary.matches("^[0-9a-f]*a365627a7a72315820[0-9a-f]{64}6c6578706572696d656e74616cf564736f6c6343[0-9a-f]{6}0040[0-9a-f]*$")){
+            solidityVersion = SolidityVersion.FROM_0_5_12_TO_0_5_15_EXPERIMENTAL;
+            String[] splitString = binary.split("(?<=a365627a7a72315820[0-9a-f]{64}6c6578706572696d656e74616cf564736f6c6343[0-9a-f]{6}0040)", 2);
+            metadata = splitString[0].substring(splitString[0].length() - 132);
+            return new Pair<>(splitString[0].substring(0, splitString[0].length() - 132), splitString[1]);
         }
 
         // From version solc-0.6.0
@@ -222,14 +262,21 @@ public class Contract {
      * @throws SolidityVersionUnknownException if the version is under 0.5.8 then it's unresolvable
      */
     public String getExactSolidityVersion() throws SolidityVersionUnknownException {
-        if (solidityVersion == SolidityVersion.UNKNOWN || solidityVersion == SolidityVersion.FROM_0_4_17_TO_0_5_8)
+        if (solidityVersion == SolidityVersion.UNKNOWN ||
+                solidityVersion == SolidityVersion.FROM_0_4_17_TO_0_5_8 ||
+                solidityVersion == SolidityVersion.FROM_0_4_17_TO_0_5_8_EXPERIMENTAL)
             throw new SolidityVersionUnknownException("Version unknown or before 0.5.9");
         String version = metadata.substring(metadata.length() - 10, metadata.length() - 4);
         int v1, v2, v3;
         v1 = Integer.parseInt(version.substring(0,2), 16);
         v2 = Integer.parseInt(version.substring(2,4), 16);
         v3 = Integer.parseInt(version.substring(4), 16);
-        return String.valueOf(v1) + '.' + v2 + '.' + v3;
+        String experimental = "";
+        if (solidityVersion == SolidityVersion.FROM_0_4_17_TO_0_5_8_EXPERIMENTAL ||
+                solidityVersion == SolidityVersion.FROM_0_5_9_TO_0_5_11_EXPERIMENTAL ||
+                solidityVersion == SolidityVersion.FROM_0_5_12_TO_0_5_15_EXPERIMENTAL)
+            experimental = " - experimental";
+        return String.valueOf(v1) + '.' + v2 + '.' + v3 + experimental;
     }
 
     /**
